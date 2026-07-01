7e3f9433bf14fcf26980f30717bf162f9c084734
xiudan320-ship-it <xiudan320@gmail.com>
2026-07-02 08:27:19 +0900

    Allow long photo notes and show publish time

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "life-vlog-site-20260701-53-pwa";
+const CACHE_NAME = "life-vlog-site-20260702-54-pwa";
 const CORE_ASSETS = [
   "./",
   "./index.html",
   "./styles.css?v=20260610-37",
   "./redesign.css?v=20260701-53",
-  "./app.js?v=20260701-53",
+  "./app.js?v=20260702-54",
   "./manifest.webmanifest",
   "./assets/app-icon-192.png",
   "./assets/app-icon-512.png",
