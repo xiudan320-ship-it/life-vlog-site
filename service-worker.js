5b17460d037ea9823e3786eae3d855725066626c
xiudan320-ship-it <xiudan320@gmail.com>
2026-07-02 21:12:26 +0900

    Update R2 upload integration and cache version

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "life-vlog-site-20260702-65-pwa";
+const CACHE_NAME = "life-vlog-site-20260702-66-pwa";
 const CORE_ASSETS = [
   "./",
   "./index.html",
   "./styles.css?v=20260610-37",
-  "./redesign.css?v=20260702-65",
-  "./app.js?v=20260702-65",
+  "./redesign.css?v=20260702-66",
+  "./app.js?v=20260702-66",
   "./manifest.webmanifest",
   "./assets/app-icon-192.png",
   "./assets/app-icon-512.png",
