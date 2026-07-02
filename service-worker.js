2b8eb4be73bd913f306ddc7a8513a1c08046941c
xiudan320-ship-it <xiudan320@gmail.com>
2026-07-02 09:33:43 +0900

    Improve wishlist views and account settings dialog

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "life-vlog-site-20260702-56-pwa";
+const CACHE_NAME = "life-vlog-site-20260702-57-pwa";
 const CORE_ASSETS = [
   "./",
   "./index.html",
   "./styles.css?v=20260610-37",
-  "./redesign.css?v=20260702-56",
-  "./app.js?v=20260702-56",
+  "./redesign.css?v=20260702-57",
+  "./app.js?v=20260702-57",
   "./manifest.webmanifest",
   "./assets/app-icon-192.png",
   "./assets/app-icon-512.png",
