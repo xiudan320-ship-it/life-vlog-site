9a076c35f5f09035d59163c525b9b0ac89f39a01
xiudan320-ship-it <xiudan320@gmail.com>
2026-07-02 15:21:32 +0900

    Add swipe navigation for photo viewer

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "life-vlog-site-20260702-63-pwa";
+const CACHE_NAME = "life-vlog-site-20260702-64-pwa";
 const CORE_ASSETS = [
   "./",
   "./index.html",
   "./styles.css?v=20260610-37",
-  "./redesign.css?v=20260702-63",
-  "./app.js?v=20260702-63",
+  "./redesign.css?v=20260702-64",
+  "./app.js?v=20260702-64",
   "./manifest.webmanifest",
   "./assets/app-icon-192.png",
   "./assets/app-icon-512.png",
