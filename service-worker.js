065547a80a1a7c0d355bcea4a6ccb7be932c5456
xiudan320-ship-it <xiudan320@gmail.com>
2026-07-03 10:03:31 +0900

    Add iframe fallback for Cloudflare migration

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "life-vlog-site-20260703-72-pwa";
+const CACHE_NAME = "life-vlog-site-20260703-73-pwa";
 const CORE_ASSETS = [
   "./",
   "./index.html",
   "./styles.css?v=20260610-37",
-  "./redesign.css?v=20260703-72",
-  "./app.js?v=20260703-72",
+  "./redesign.css?v=20260703-73",
+  "./app.js?v=20260703-73",
   "./manifest.webmanifest",
   "./assets/app-icon-192.png",
   "./assets/app-icon-512.png",
