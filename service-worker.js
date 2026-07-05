0cd691566c360f1ae625a26390b9fd4ea27b86f0
xiudan320-ship-it <xiudan320@gmail.com>
2026-07-05 13:43:50 +0900

    Move desktop secret controls to sidebar

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "life-vlog-site-20260705-118-pwa";
+const CACHE_NAME = "life-vlog-site-20260705-119-pwa";
 const CORE_ASSETS = [
   "./",
   "./index.html",
   "./styles.css?v=20260610-37",
-  "./redesign.css?v=20260705-118",
-  "./app.js?v=20260705-118",
+  "./redesign.css?v=20260705-119",
+  "./app.js?v=20260705-119",
   "./manifest.webmanifest",
   "./assets/food-wheel-icon.png",
   "./assets/app-icon-192.png",
