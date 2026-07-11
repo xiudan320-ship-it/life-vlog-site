978a49693e0c12f7c3cbd47cf9a8461e12590e77
xiudan320-ship-it <xiudan320@gmail.com>
2026-07-11 09:44:26 +0900

    Stabilize detail navigation and add encrypted backups

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "life-vlog-site-20260711-152-pwa";
+const CACHE_NAME = "life-vlog-site-20260711-153-pwa";
 const APP_MEDIA_CACHES = new Set([
   "life-vlog-diary-media-cache",
   "life-vlog-secret-media-cache",
@@ -7,8 +7,8 @@ const CORE_ASSETS = [
   "./",
   "./index.html",
   "./styles.css?v=20260610-37",
-  "./redesign.css?v=20260711-152",
-  "./app.js?v=20260711-152",
+  "./redesign.css?v=20260711-153",
+  "./app.js?v=20260711-153",
   "./manifest.webmanifest",
   "./assets/food-wheel-icon.png",
   "./assets/app-icon-192.png",
